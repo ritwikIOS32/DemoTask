5afc76d4d6a7e8124bd956243af16ecd2fa6ad95
RitwikHanRiver <ritwik@divii.com>
2022-08-25 14:16:49 +0530

    first commit

---
+++ PWCTask/HomeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCollectionViewCell.swift
+//  PWCTask
+//
+//  Created by Hanriver MacBook on 24/08/22.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ PWCTask/TopCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TopCollectionViewCell.swift
+//  PWCTask
+//
+//  Created by Hanriver MacBook on 24/08/22.
+//
+
+import UIKit
+
+class TopCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
